d3fe842102a09cf64ebd5c71aa56dd882433ee34
Ilya Lebedev <bacherko@gmail.com>
2021-04-29 22:04:39 +0300

    add router.push example

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div style={{display:'flex', flexDirection:'column'}}>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
       <Link href={{query: {hello: 'world'}}}><a>Home with param</a></Link>
       <Link href={{query: {hello: ''}}}><a>Home with empty param</a></Link>
       <Link href={{query: {hello: undefined}}}><a>Home without param</a></Link>
+
+      <button type="button" onClick={e => {
+        e.preventDefault();
+
+        router.push({
+          query: {
+            hello: undefined
+          }
+        })
+      }}>Same problem with router.push(...)
+      </button>
     </div>
   )
 }
